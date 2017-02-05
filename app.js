05f622e540adffb730c2c86664753a1ad8d69699
Tom Scott <tomscott85@gmail.com>
2017-02-05 14:28:54 +1100

    static files using middleware

--- app.js
+++ app.js
@@ -3,4 +3,19 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send('<html><head><link href = assets/style.css type=text/css rel=stylesheet></head><body>Hello World</body>');
+});
+
+app.get('/person/:id', function(req, res) {
+  res.send('<html><head></head><body>Person: ' +
+  req.params.id + '</body>');
+});
+
+app.get('/api', function (req, res) {
+  res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
 app.listen(port);
