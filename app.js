de315999b05a0842e047b53f61e85db4cd3a3ef9
Tom Scott <tomscott85@gmail.com>
2017-02-05 14:44:55 +1100

    templates / view engines

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
+app.use('/', function(req, res, next) {
+  console.log('Request Url: ', req.url);
+  next();
+});
+
 app.get('/', function(req, res) {
-  res.send('<html><head><link href = assets/style.css type=text/css rel=stylesheet></head><body>Hello World</body>');
+  res.render('index');
 });
 
 app.get('/person/:id', function(req, res) {
-  res.send('<html><head></head><body>Person: ' +
-  req.params.id + '</body>');
+  res.render('person', { ID: req.params.id });
 });
 
 app.get('/api', function (req, res) {
